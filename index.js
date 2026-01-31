00fb1399d54a479a6e4fb7f06e5d11793dddcd76
charitha-m <modiyamcharitha22@gmail.com>
2026-01-31 21:06:34 +0530

    added vehicle route in index file

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import userRouter from "./routes/user.routes.js";
 import { logger } from "./middlewares/logger.middleware.js";
+import vehicleRouter from "./routes/vehicle.routes.js";
 
 
 const app = express();
@@ -8,6 +9,8 @@ app.use(logger)
 app.use(express.json());
 
 app.use("/users", userRouter)
+app.use("/vehicles", vehicleRouter)
+app.use("/trip")
 
 app.use((req,res)=>{
     res.status(404).json({msg: "This Request is not found"});
